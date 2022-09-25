5c7b8912557e8d1c0947a62739bbe5f386d2a23a
yuuttana1223 <yutanadreamfilel.seed@gmail.com>
2022-09-25 15:19:35 +0900

    ✨ user一覧取得のQueryを追加

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -5,13 +5,11 @@ module Types
     include GraphQL::Types::Relay::HasNodesField
 
     # Add root-level fields here.
+    # ドキュメントでは[Types::UserType]とあるが省略できそう
+    field :users, [UserType], null: false
     # They will be entry points for queries on your schema.
-
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World!"
+    def users
+      User.all
     end
   end
 end
