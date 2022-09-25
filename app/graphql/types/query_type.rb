8dbc17ee919f8960b4a2967673340f9968f33718
yuuttana1223 <yutanadreamfilel.seed@gmail.com>
2022-09-25 16:37:11 +0900

    ✨ user詳細取得のQueryを追加

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -6,10 +6,20 @@ module Types
 
     # Add root-level fields here.
     # ドキュメントでは[Types::UserType]とあるが省略できそう
+    # 複数形の[UserType]
     field :users, [UserType], null: false
+    # 単数系UserType
+    field :user, UserType, "Find a user by ID" do
+      argument :id, ID
+    end
     # They will be entry points for queries on your schema.
     def users
       User.all
+    end
+
+    # キーワード引数 呼び出す側はuser(id: 1)みたいな呼び方
+    def user(id:)
+      User.find(id)
     end
   end
 end
