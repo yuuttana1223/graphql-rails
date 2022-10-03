a4f173304694ad9a846b81de4f4256baadb33c7f
yuuttana1223 <yutanadreamfilel.seed@gmail.com>
2022-10-03 20:41:49 +0900

    ✨ graphqlでassociationを実装

--- app/graphql/types/post_type.rb
+++ app/graphql/types/post_type.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
 module Types
-  class UserType < Types::BaseObject
+  class PostType < Types::BaseObject
     field :id, ID, null: false
-    field :name, String, null: false
-    field :email, String
-    field :posts, [Types::PostType], null: false
+    field :title, String
+    field :body, String
+    field :user_id, Integer, null: false
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
   end
