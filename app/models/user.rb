68b8e066a36a3c940c0de53a4fe7dbe6c5395d76
yuuttana1223 <yutanadreamfilel.seed@gmail.com>
2022-10-03 19:35:16 +0900

    ✨ Postモデルを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ApplicationRecord
   validates :name, length: { maximum: 20 }
+  has_many :posts
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_25_045625) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_03_103215) do
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.string "body"
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
@@ -18,4 +27,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_25_045625) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "posts", "users"
 end
