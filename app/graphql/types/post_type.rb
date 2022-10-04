906492d3e11d2754bb3070c5cd246b74a2ac6285
yuuttana1223 <yutanadreamfilel.seed@gmail.com>
2022-10-04 19:07:12 +0900

    ✨ postからもuserが取れるように更新

--- app/graphql/types/post_type.rb
+++ app/graphql/types/post_type.rb
@@ -6,6 +6,7 @@ module Types
     field :title, String
     field :body, String
     field :user_id, Integer, null: false
+    field :user, Types::UserType, null: false
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
   end
