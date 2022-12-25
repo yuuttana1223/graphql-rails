819a7b87b6da58391d2a5e018f57cf2c2a9a615b
yuuttana1223 <64090349+yuuttana1223@users.noreply.github.com>
2022-12-25 22:11:08 +0900

    🚧

--- app/graphql/graphql_rails_schema.rb
+++ app/graphql/graphql_rails_schema.rb
@@ -2,8 +2,7 @@ class GraphqlRailsSchema < GraphQL::Schema
   mutation(Types::MutationType)
   query(Types::QueryType)
 
-  # For batch-loading (see https://graphql-ruby.org/dataloader/overview.html)
-  use GraphQL::Dataloader
+  use GraphQL::Batch
 
   # GraphQL-Ruby calls this when something goes wrong while running a query:
   def self.type_error(err, context)
